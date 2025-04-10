d3419883b4e86decdbbf6823078d91e06513b46b
heitor-sanches <150952274+heitor-sanches@users.noreply.github.com>
2025-04-10 01:24:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const axios = require('axios');
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 app.use(express.json());
 
 const CadastroSchema = new mongoose.Schema({
